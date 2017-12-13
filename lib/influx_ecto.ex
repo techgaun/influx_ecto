a742354acef94c5bcde1693df73361ab616e212f
techgaun <coolsamar207@gmail.com>
2017-12-13 15:45:50 -0600

    initial skeleton

---
+++ lib/influx_ecto.ex
@@ -0,0 +1,18 @@
+defmodule InfluxEcto do
+  @moduledoc """
+  Documentation for InfluxEcto.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> InfluxEcto.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/influx_ecto_test.exs
@@ -0,0 +1,8 @@
+defmodule InfluxEctoTest do
+  use ExUnit.Case
+  doctest InfluxEcto
+
+  test "greets the world" do
+    assert InfluxEcto.hello() == :world
+  end
+end
