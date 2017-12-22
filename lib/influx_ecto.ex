d7c7a316b20adabe5c4a81e6f83ea9535d73b86c
techgaun <coolsamar207@gmail.com>
2017-12-22 11:08:26 -0600

    few little addition

--- lib/influx_ecto.ex
+++ lib/influx_ecto.ex
@@ -7,5 +7,25 @@ defmodule InfluxEcto do
 
   @behaviour Ecto.Adapter
 
+  @timestamp_unit :millisecond
+
+  @doc false
   defmacro __before_compile__(_opts), do: :ok
+
+  @doc """
+  Make sure all the necessary apps for InfluxEcto to run are started.
+  """
+  def ensure_all_started(_repo, type) do
+    {:ok, _} = Application.ensure_all_started(:instream, type)
+
+    {:ok, [:instream]}
+  end
+
+  @doc """
+  Returns the childspec that starts the adapter process.
+  """
+  def child_spec(_repo, _opts), do: Supervisor.Spec.supervisor(Supervisor, [[], [strategy: :one_for_one]])
+
+  @doc false
+  def autogenerate(_), do: System.system_time(@timestamp_unit)
 end
